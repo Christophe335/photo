8f8c7f7fc88ab030debfddbdf0020fe4162276ef
Christophe335 <christophemouillet335@hotmail.com>
2025-11-10 22:26:28 +0100

    Ajout des images produits - Début de mise en forme

--- pages/livre-photo.php
+++ pages/livre-photo.php
@@ -14,5 +14,21 @@
         </div>
     </div>
 </main>
-
+<section>
+    <div class="container">
+        <h2 class="title-h3 text-center">Service de personnalisation</h2>
+        <h3 class="title-h2 text-center">Peel & Stick Sheets</h3>
+        <div class="ligne">
+            <div class="colonne-1 onleft">
+                <img src="../images/produits/peel-stick 1.webp" alt="Image en avant de feuilles Peel & Stick Sheets">
+            </div>
+            <div class="colonne-2 onright">
+                <p>Cette solution prête à l'emploi dispose d'une zone auto-adhésive qui vous permet de coller n'importe quel type d'impression directement dans notre produit. Pas de situation collante avec de la colle renversée pour vous ! Une innovation intelligente et unique qui simplifiera votre vie. Imprimez vos photos et créez votre propre livre photo / panneau en un rien de temps. Le cadeau idéal pour quelqu'un de très spécial.Entièrement personnalisable avec l'œuvre de votre choix. Améliorez vos produits en leur donnant la touche finale que vos clients se souviendront. Ajoutez une touche personnelle pour en faire de puissants outils de marque avec votre propre design d'entreprise ou une œuvre en couleur pleine de paillettes. Sortez du lot. Boostez la notoriété de premier plan.</p>
+            </div>
+        </div>
+        <div class="container">
+            <img src="../images/produits/peel-stick-utilisation1.webp" alt="4 images montrant l'utilisation des Peel & Stick Sheets">
+        </div>
+    </div>
+</section>
 <?php include '../includes/footer.php'; ?>
