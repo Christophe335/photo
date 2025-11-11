fee8b156b6188baf96d6a7a1245c963d544472b4
Christophe335 <christophemouillet335@hotmail.com>
2025-11-11 16:06:59 +0100

    Essai de mise en place des éléments dans la page livre photo

--- includes/bt-devis.php
+++ includes/bt-devis.php
@@ -1,5 +1,6 @@
 <section class="centre-div">
     <p class="centre-text">Vous souhaitez un produit personnalisé ou une impression sur mesure ?</p>
+    <p class="centre-text">Nous imprimons vos photos.</p>
     <div style="text-align: center;">
         <a class="bouton" href="../formulaires/devis.php">Demander un devis</a>
     </div>
--- pages/livre-photo.php
+++ pages/livre-photo.php
@@ -7,17 +7,12 @@
         <p style="text-align: center; font-size: 18px; color: #666; margin-bottom: 40px;">
             Transformez vos photos en livre photo professionnel de haute qualité.
         </p>
-<?php include '../includes/bt-devis.php'; ?>
-        <div class="content">
-            <!-- Contenu spécifique à la page livre photo -->
-            <p>Contenu de la page livre photo à développer...</p>
-        </div>
     </div>
 </main>
-<section>
+<section class="section1">
     <div class="container">
-        <h2 class="title-h3 text-center">Service de personnalisation</h2>
-        <h3 class="title-h2 text-center">Peel & Stick Sheets</h3>
+        <h2 class="title-h3 centre-text">Service de personnalisation</h2>
+        <h3 class="title-h2 centre-text">Peel & Stick Sheets</h3>
         <div class="ligne">
             <div class="colonne-1 onleft">
                 <img src="../images/produits/peel-stick 1.webp" alt="Image en avant de feuilles Peel & Stick Sheets">
@@ -29,6 +24,119 @@
         <div class="container">
             <img src="../images/produits/peel-stick-utilisation1.webp" alt="4 images montrant l'utilisation des Peel & Stick Sheets">
         </div>
+        <div class="container tableau-produit centre-div" style="width: 740px;">
+            <p class="id" id="peel-stick-sheets">Famille test</p>
+            <p class="code">Code<span style="margin-left: 100px;">Description</span> <span style="margin-left: 150px;">Conditionnement</span> <span style="margin-left: 40px;">Prix HT</span><span style="margin-left: 20px;">Quantité</span></p>
+            <p>00000001 &emsp;<span>produit test 1 dimension format</span><span style="margin-left: 75px;">20</span><span style="margin-left: 85px;">20€</span><span style="margin-left: 30px;">- [] +</span>&emsp;<span class="bouton-ajout">Ajouter au Panier</span></p>
+            <p>00000002 &emsp;<span>produit test 2 dimension format</span><span style="margin-left: 75px;">20</span><span style="margin-left: 85px;">20€</span><span style="margin-left: 30px;">- [] +</span>&emsp;<span class="bouton-ajout">Ajouter au Panier</span></p>
+            </br>
+        </div>
     </div>
+    <?php include '../includes/bt-devis.php'; ?>
 </section>
+<section class="section2">
+    <div class="container">
+        <h2 class="title-h3 centre-text">Livres photo autocollants - Couverture couleur</h2>
+        <p>Produit à faire soi-même. Laissez vos clients personnaliser eux-mêmes leurs photos préférées, sans colle ni ruban adhésif !
+ Étape 1: retirez le ﬁlm protecteur de la couche adhésive de la couverture.  Étape 2: collez votre photo.
+En un rien de temps, les clients créent leur propre souvenir ou cadeau personnalisé.</p>
+    </div>
+    <div class="ligne">
+        <div class="colonne-3 onleft">
+            <img src="../images/produits/livrephotoautocollant-1.webp" alt="Livre photo autocollant de couleur vert pâle montrant le résultat de l'application des Peel & Stick Sheets sur la couverture">
+        </div>
+        <div class="colonne-3 onright">
+            <img src="../images/produits/livrephotoautocollant-3.webp" alt="Livre photo autocollant ouvert, de couleur bleu pâle montrant le résultat de l'application des Peel & Stick Sheets sur l'intérieur de l'album">
+        </div>
+    </div>
+    <div class="ligne centre-div" style="width:500px;">
+        <img src="../images/produits/livrephotoautocollant-couleur1.webp" alt="Album photo peel & stick fermé de couleur bleu pâle">
+        <img src="../images/produits/livrephotoautocollant-couleur2.webp" alt="Album photo peel & stick fermé de couleur vert pâle">
+        <img src="../images/produits/livrephotoautocollant-couleur3.webp" alt="Album photo peel & stick fermé de couleur violet pâle">
+        <img src="../images/produits/livrephotoautocollant-couleur4.webp" alt="Album photo peel & stick fermé de couleur jaune pâle">
+        <img src="../images/produits/livrephotoautocollant-couleur5.webp" alt="Album photo peel & stick fermé de couleur rose pâle">
+    </div>
+</br>
+<div class="container tableau-produit centre-div" style="width: 740px;">
+            <p class="id" id="peel-stick-sheets">Famille test</p>
+            <p class="code">Code<span style="margin-left: 100px;">Description</span> <span style="margin-left: 150px;">Conditionnement</span> <span style="margin-left: 40px;">Prix HT</span><span style="margin-left: 20px;">Quantité</span></p>
+            <p>00000001 &emsp;<span>produit test 1 dimension format</span><span style="margin-left: 75px;">20</span><span style="margin-left: 85px;">20€</span><span style="margin-left: 30px;">- [] +</span>&emsp;<span class="bouton-ajout">Ajouter au Panier</span></p>
+            <p>00000002 &emsp;<span>produit test 2 dimension format</span><span style="margin-left: 75px;">20</span><span style="margin-left: 85px;">20€</span><span style="margin-left: 30px;">- [] +</span>&emsp;<span class="bouton-ajout">Ajouter au Panier</span></p>
+            </br>
+        </div>
+        </br>
+    <?php include '../includes/bt-devis.php'; ?>
+</section>
+<section class="section1">
+    <div class="container">
+        <h2 class="title-h3 centre-text">Livres photo autocollants - Couverture Noir</h2>
+        <p>Créez facilement de magnifiques cadeaux photo en quelques secondes avec un pratique livre photo Peel & Stick. 
+Imprimez simplement vos photos et créez votre propre livre photo en un rien de temps. 
+Ces livres photo lay-flat sont sans aucun doute faciles à assembler et offrent une tenue solide et robuste. 
+Le cadeau idéal pour une personne très spéciale.</p>
+    </div>
+    <div class="ligne">
+        <div class="colonne-2">
+            <div class="container tableau-produit centre-div" style="width: 740px;">
+                <p class="id" id="peel-stick-sheets">Famille test</p>
+                <p class="code">Code<span style="margin-left: 100px;">Description</span> <span style="margin-left: 150px;">Conditionnement</span> <span style="margin-left: 40px;">Prix HT</span><span style="margin-left: 20px;">Quantité</span></p>
+                <p>00000001 &emsp;<span>produit test 1 dimension format</span><span style="margin-left: 75px;">20</span><span style="margin-left: 85px;">20€</span><span style="margin-left: 30px;">- [] +</span>&emsp;<span class="bouton-ajout">Ajouter au Panier</span></p>
+                <p>00000002 &emsp;<span>produit test 2 dimension format</span><span style="margin-left: 75px;">20</span><span style="margin-left: 85px;">20€</span><span style="margin-left: 30px;">- [] +</span>&emsp;<span class="bouton-ajout">Ajouter au Panier</span></p>
+                </br>
+            </div>
+            <img src="../images/produits/album-photo-autocollant-1.webp" alt="Livre photo autocollant ouvert, de couleur noir montrant le résultat de l'application des Peel & Stick Sheets sur l'intérieur de l'album">
+        </div>
+        <div class="colonne-1 onright">
+            <img src="../images/produits/album-photo-autocollant-2.webp" alt="Livre photo autocollant de couleur noir montrant le résultat de l'application des Peel & Stick Sheets sur la couverture">
+        </div>
+    </div>
+        </br>
+    <?php include '../includes/bt-devis.php'; ?>
+</section>
+<section class="section2">
+    <div class="container">
+        <h2 class="title-h3 centre-text">Livre Photo Créatif</h2>
+    </div>
+    <div class="ligne">
+        <div>
+            <p>Une image en dit plus que mille mots. Dans ces livres photo
+            instantanés colorés, il y a de la place pour 10 images de ce type (soit
+            plus de 10 000 mots) et pour ajouter un message spécial. Combien
+            de mots cela représente-t-il ? Nous avons perdu le compte ...</p>
+            <p>Indispensable pour les créateurs de scrapbooking
+            Les livres photo instantanés constituent la base idéale pour un
+            scrapbook, une sorte de livre photo à l'ancienne, mais un peu plus
+            personnel. Ils comprennent des autocollants double face pour fixer
+            des photos et d'autres souvenirs, ainsi qu'un marqueur pour ajouter
+            des noms, des dates, des citations ou d'autres éléments d'écriture</p>
+        </div>
+        <div class="onright">
+            <img src="../images/produits/livrephotocreatif-1.webp" alt="Livre photo créatif fermé montrant une couverture personnalisée avec des photos collées dessus">
+        </div>
+    </div>
+    <div class="ligne">
+        <div class="colonne-1">
+            <img src="../images/produits/instant-photo-book-1.webp" alt="Présentation du livre photo créatif sous la forme de sa pochette contenant tout le néccessaire pour créer le livre photo">
+        </div>
+        <div>
+        <div class="colonne-2 ligne">
+            <div><img style="width: 80px; height: 80px; margin-right: 25px;" src="../images/produits/livrephotocreatif-couleur1.webp" alt="Exmple de la couleur noire disponible pour le livre créatif"></br>Black</div>
+            <div><img style="width: 80px; height: 80px; margin-right: 25px;" src="../images/produits/livrephotocreatif-couleur2.webp" alt="Exmple de la couleur bleue disponible pour le livre créatif"></br>Blue</div>
+            <div><img style="width: 80px; height: 80px; margin-right: 25px;" src="../images/produits/livrephotocreatif-couleur3.webp" alt="Exmple de la couleur rose disponible pour le livre créatif"></br>Pink</div>
+            <div><img style="width: 80px; height: 80px; margin-right: 25px;" src="../images/produits/livrephotocreatif-couleur4.webp" alt="Exmple de la couleur violette disponible pour le livre créatif"></br>Purple</div>
+            <div><img style="width: 80px; height: 80px; margin-right: 25px;" src="../images/produits/livrephotocreatif-couleur5.webp" alt="Exmple de la couleur blanche disponible pour le livre créatif"></br>White</div>
+        </div>
+            <div class="container tableau-produit centre-div" style="width: 740px;">
+                <p class="id" id="peel-stick-sheets">Famille test</p>
+                <p class="code">Code<span style="margin-left: 100px;">Description</span> <span style="margin-left: 150px;">Conditionnement</span> <span style="margin-left: 40px;">Prix HT</span><span style="margin-left: 20px;">Quantité</span></p>
+                <p>00000001 &emsp;<span>produit test 1 dimension format</span><span style="margin-left: 75px;">20</span><span style="margin-left: 85px;">20€</span><span style="margin-left: 30px;">- [] +</span>&emsp;<span class="bouton-ajout">Ajouter au Panier</span></p>
+                <p>00000002 &emsp;<span>produit test 2 dimension format</span><span style="margin-left: 75px;">20</span><span style="margin-left: 85px;">20€</span><span style="margin-left: 30px;">- [] +</span>&emsp;<span class="bouton-ajout">Ajouter au Panier</span></p>
+                </br>
+            </div>
+        </div>
+    </div>
+    </br>
+    <?php include '../includes/bt-devis.php'; ?>
+</section>
+
 <?php include '../includes/footer.php'; ?>
